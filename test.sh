b6ee4c345e8bbf89a92141cc1c4ae88b8082bdf8
ricky han <rickylqhan@gmail.com>
2020-10-17 04:28:10 -0400

    remove upow

--- test.sh
+++ test.sh
@@ -9,6 +9,5 @@ RUST_LOG=debug ./target/debug/solenoid tests/contracts/set.sol
 
 llc out.ll -o out.x64.s -O3
 llc out.ll -filetype=obj -o out.o -relocation-model=pic -O3
-llc runtime/upow.ll -filetype=obj -o upow.o -relocation-model=pic -O3
-clang runtime/rt.c runtime/sha3.c runtime/utils.c out.o upow.o -fPIC -o a.out
+clang runtime/rt.c runtime/sha3.c runtime/utils.c out.o -fPIC -o a.out
 ./a.out
